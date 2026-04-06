f610d53b063dfc3ad7e8429abeb1ca0f6c7000bd
Lo-kith <lokithmlokith@email.com>
2026-04-06 14:54:04 +0530

    refactor:add some datas

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const { title } = require("node:process");
 
 app.get("/get",(req,res)=>{
     res.send("Hello World");
 })
 
+const container=[
+    {id:1,title:"Container A",color:"#FF5733",content:"Data from server 1"},
+    {id:2,title:"Container B",color:"#33FF57",content:"Data from server 2"},
+    {id:3,title:"Container C",color:"#3357FF",content:"Data from server 3"},
+    {id:4,title:"Container D",color:"#F333FF",content:"Data from server 4"},
+    {id:5,title:"Container E",color:"#FF33A8",content:"Data from server 5"},
+]
+app.get("/container",(req,res)=>{
+    res.json(container);
+})
 const PORT=3000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
